feba2fa3c4af548f6f864fd70c4f5aa57f251881
SimonBara <simobara@hotmail.it>
2025-12-10 11:36:03 -0500

    top infos mobile middle bottom

--- src/Ap/App.jsx
+++ src/Ap/App.jsx
@@ -10,10 +10,11 @@ import StandingsPage from "../Body/1standingsPage";
 import GroupStagePage from "../Body/2groupStagePage";
 import TablePage from "../Body/3tablePage";
 
+
 const App = () => {
   return (
     <Router>
-      <div className="relative h-[95vh] md:h-screen w-screen bg-sky-950 overflow-hidden">
+      <div className="relative h-[100svh] md:h-screen w-screen bg-sky-950 overflow-hidden overscroll-none touch-none">
         <TopInfo />
         <div className="h-full w-full flex">
           <Routes>
