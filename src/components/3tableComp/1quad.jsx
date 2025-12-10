68e0f25d3b22043bfbf358abafaaccd504cc8938
SimonBara <simobara@hotmail.it>
2025-12-10 13:39:22 -0500

    TableComp containers mobile

--- src/components/3tableComp/1quad.jsx
+++ src/components/3tableComp/1quad.jsx
@@ -1,6 +1,6 @@
 const Quadrato = ({ label }) => {
   return (
-    <div className="relative w-16 h-16 bg-sky-800 border-x-2 border-y-4 border-gray-700 rounded-[16px] shadow-xl flex items-center justify-center">
+    <div className="relative w-16 md:h-16 h-12 bg-sky-800 border-x-2 border-y-4 border-gray-700 rounded-[16px] shadow-xl flex items-center justify-center">
       <span className="absolute md:-top-1 -top-2 left-1/2 -translate-x-1/2 text-gray-400 text-xs font-bold">
         {label}
       </span>
