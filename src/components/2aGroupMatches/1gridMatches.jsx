bea022d082c0419ca1ed9c4e63b5597496ef7d90
SimonBara <simobara@hotmail.it>
2025-12-21 10:18:40 -0500

    gridMatches sorting class mobile view

--- src/components/2aGroupMatches/1gridMatches.jsx
+++ src/components/2aGroupMatches/1gridMatches.jsx
@@ -408,7 +408,7 @@ export default function GridMatchesPage() {
     max-h-[80vh] overflow-auto
     rounded-2xl
     bg-purple-400
-    p-1
+    md:mr-0 mr-2
     ${
       mobileSide === "left"
         ? CENTRAL_GROUPS_MOBILE.has(mobileGroup)
