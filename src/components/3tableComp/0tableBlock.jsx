3a612a943ae06297fd5d72f7ff9067a7d6031b8f
SimonBara <simobara@hotmail.it>
2026-01-15 09:59:48 -0500

    refining tableBlocks mobile

--- src/components/3tableComp/0tableBlock.jsx
+++ src/components/3tableComp/0tableBlock.jsx
@@ -500,7 +500,7 @@ const TableBlock = ({ isLogged }) => {
       >
         {/* ✅ COLONNA 32 A */}
         <div className="relative flex-1 h-full bg-purple flex flex-col items-center md:pt-20 pt-12">
-          <div className="md:-mt-8 -mt-4">
+          <div className="md:-mt-8 -mt-6">
             {renderMatchBlock(mA1, Rett.A, "round32")}
           </div>
           <div>
